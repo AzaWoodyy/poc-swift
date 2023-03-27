4aea92420d43b365ebf97c6f610e81f8390e11aa
AzaWoodyy <aza.woodyy@gmail.com>
2023-03-27 10:58:09 +0200

    Initial Commit

---
+++ NpmApi/NpmApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NpmApiApp.swift
+//  NpmApi
+//
+//  Created by Enzo Neault on 27/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct NpmApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
